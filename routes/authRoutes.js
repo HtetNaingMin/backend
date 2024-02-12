c16f0cbbf105e4e9b80d7ab82c1406064fe4269b
John <147623698+HtetNaingMin@users.noreply.github.com>
2024-02-13 02:15:41 +0800

    changes 5.1

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,7 +11,9 @@ module.exports = (isAuthenticated) => {
     try {
       const currentTime = Date.now();
       const sessionExpiry = req.session.cookie.expires.getTime();
-  
+      console.log("Session:", req.session);
+      console.log("User ID:", req.session.userId);
+      
       if (currentTime > sessionExpiry) {
         // Session has expired
         res.status(401).json({ error: "Session Expired" });
@@ -33,55 +35,74 @@ module.exports = (isAuthenticated) => {
   router.post('/login', (req, res) => {
     const { username, password } = req.body;
     const query = 'SELECT * FROM User WHERE Username = ? AND password = ?';
-  
+    
     db.get(query, [username, password], (err, user) => {
-      if (err) {
-        console.error('Database query error:', err.message);
-        res.status(500).json({ error: 'Internal Server Error' });
-        return;
-      }
-  
-      if (user) {
-        console.log('User:', user);
-  
-        if (user.UserType === 'nutritionist') {
-          // Check NutritionistSignUp table for approval status
-          const nutritionistQuery = 'SELECT * FROM NutritionistSignUp WHERE UserID = ?';
-          db.get(nutritionistQuery, [user.UserID], (err, nutritionistData) => {
-            if (err) {
-              console.error('Database query error:', err.message);
-              res.status(500).json({ error: 'Internal Server Error' });
-              return;
-            }
-  
-            if (nutritionistData && nutritionistData.Status === 'approved') {
-              console.log('Login successful for approved nutritionist');
-              req.session.user = { ...user, userType: 'nutritionist', status: 'approved' };
-              res.json({ message: 'Login successful', user: req.session.user });
-            } else if (nutritionistData && nutritionistData.Status !== 'approved') {
-              console.log('Nutritionist account is pending approval');
-              res.status(401).json({ error: 'Nutritionist account is pending approval' });
-            } else {
-              console.log('Nutritionist account not found');
-              res.status(401).json({ error: 'Invalid credentials for a nutritionist account' });
-            }
-          });
-        } else if (user.UserType === 'system admin') {
-          console.log('Login successful for system admin');
-          req.session.user = { ...user, userType: 'system admin' };
-          res.json({ message: 'Login successful', user: req.session.user });
-        } else {
-          console.log('Login successful for non-nutritionist user');
-          // For non-nutritionist users, proceed with login
-          req.session.user = { ...user, userType: 'user' };
-          res.json({ message: 'Login successful', user: req.session.user });
+        if (err) {
+            console.error('Database query error:', err.message);
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
         }
-      } else {
-        console.log('Invalid credentials');
-        res.status(401).json({ error: 'Invalid credentials' });
-      }
+
+        if (user) {
+            console.log('User:', user);
+
+            if (user.UserType === 'nutritionist') {
+                // Check NutritionistSignUp table for approval status
+                const nutritionistQuery = 'SELECT * FROM NutritionistSignUp WHERE UserID = ?';
+                db.get(nutritionistQuery, [user.UserID], (err, nutritionistData) => {
+                    if (err) {
+                        console.error('Database query error:', err.message);
+                        res.status(500).json({ error: 'Internal Server Error' });
+                        return;
+                    }
+
+                    if (nutritionistData && nutritionistData.Status === 'approved') {
+                        console.log('Login successful for approved nutritionist');
+                        req.session.userId = user.UserID;
+                        res.cookie('connect.sid', req.sessionID, {
+                            maxAge: null,/* set your desired max age in milliseconds */
+                            httpOnly: true,
+                            secure: process.env.NODE_ENV === 'production', // Set to true if using HTTPS in production
+                            sameSite: 'None', // Required for cross-site cookies
+                        });
+                        res.json({ message: 'Login successful', user, userType: 'nutritionist', status: 'approved' });
+                    } else if (nutritionistData && nutritionistData.Status !== 'approved') {
+                        console.log('Nutritionist account is pending approval');
+                        res.status(401).json({ error: 'Nutritionist account is pending approval' });
+                    } else {
+                        console.log('Nutritionist account not found');
+                        res.status(401).json({ error: 'Invalid credentials for a nutritionist account' });
+                    }
+                });
+            } else if (user.UserType === 'system admin') {
+                console.log('Login successful for system admin');
+                req.session.userId = user.UserID;
+                res.cookie('connect.sid', req.sessionID, {
+                    maxAge: null,/* set your desired max age in milliseconds */
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === 'production', // Set to true if using HTTPS in production
+                    sameSite: 'None', // Required for cross-site cookies
+                });
+                res.json({ message: 'Login successful', user, userType: 'system admin' });
+            } else {
+                console.log('Login successful for non-nutritionist user');
+                // For non-nutritionist users, proceed with login
+                req.session.userId = user.UserID;
+                res.cookie('connect.sid', req.sessionID, {
+                    maxAge: null,/* set your desired max age in milliseconds */
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === 'production', // Set to true if using HTTPS in production
+                    sameSite: 'None', // Required for cross-site cookies
+                });
+                res.json({ message: 'Login successful', user, userType: 'user' });
+            }
+        } else {
+            console.log('Invalid credentials');
+            res.status(401).json({ error: 'Invalid credentials' });
+        }
+
     });
-  });
+});
   
 
   // Route for user logout
