1bbf013ee6816639d1eb45b2654067b93daf3bad
jannath2001 <116989317+jannath2001@users.noreply.github.com>
2024-02-13 13:33:39 +0800

    Update server.js

--- server.js
+++ server.js
@@ -50,7 +50,7 @@
 
   app.use(
     cors({
-      origin: "https://fyp-webapp-six.vercel.app", // Adjust this to your actual Next.js app origin
+      origin: "https://cleancookbook.vercel.app", // Adjust this to your actual Next.js app origin
       methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
       optionsSuccessStatus: 204,
       credentials: true,
