4b6be773f9d4730d409cc8a695aa9c6d22827a82
jannath2001 <116989317+jannath2001@users.noreply.github.com>
2024-02-13 15:45:24 +0800

    Update authMiddleware.js

--- authMiddleware.js
+++ authMiddleware.js
@@ -1,12 +1,25 @@
 // authMiddleware.js
+const jwt = require('jsonwebtoken');
+const secretKey = 'yourLongLivedSecretKey'; // Replace with your actual secret key
+
 const isAuthenticated = (req, res, next) => {
-    if (req.session && req.session.userId) {
-      req.userId = req.session.userId;
-      return next();
-    } else {
-      res.status(401).json({ error: 'Not authenticated' });
+  // Assuming you stored the token in a client-side storage (e.g., localStorage)
+  const token = req.headers.authorization; // Adjust based on your setup
+
+  if (!token) {
+    return res.status(401).json({ error: 'Not authenticated' });
+  }
+
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: 'Invalid token' });
     }
-  };
-  
-  module.exports = isAuthenticated;
+
+    req.userId = decoded.userId; // Add userId to the request for further use
+    next();
+  });
+};
+
+module.exports = isAuthenticated;
+
   
