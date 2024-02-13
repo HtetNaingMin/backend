f7029eedde77432ca3f9a1fe550ed0338bea34b8
jannath2001 <jannath2001@gmail.com>
2024-02-13 23:33:18 +0800

    push

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,30 +4,28 @@ const router = express.Router();
 const db = require('../db'); // Import your database module here
 const crypto = require('crypto');
 
+const secretKey = crypto.randomBytes(32).toString('hex');
+
 // Middleware to check if the user is authenticated
 module.exports = (isAuthenticated) => {
   // Route to check if the user is authenticated
-  router.get("/api/check-auth", isAuthenticated, (req, res) => {
+  router.get('/check-auth', (req, res) => {
+    // Get the token from the Authorization header
+    const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
+    console.log (token);
+
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
     try {
-      const currentTime = Date.now();
-      const sessionExpiry = req.session.cookie.expires.getTime();
-      console.log("Session:", req.session);
-      console.log("User ID:", req.session.userId);
-      
-      if (currentTime > sessionExpiry) {
-        // Session has expired
-        res.status(401).json({ error: "Session Expired" });
-        return;
-      }
-  
-      // Your authentication logic here
-  
-      // If authenticated, return the appropriate response
-      // If not authenticated, consider throwing an error
-      // res.status(401).json({ error: "Unauthorized" });
+      // Verify the token
+      const decoded = jwt.verify(token, secretKey);
+      req.userId = decoded.userId; // Add userId to the request for further use
+      return isAuthenticated(req, res);
     } catch (error) {
-      console.error("Error in /api/check-auth route:", error);
-      res.status(500).json({ error: "Internal Server Error" });
+      console.error('JWT verification error:', error.message);
+      return res.status(401).json({ error: 'Unauthorized' });
     }
   });
 
@@ -35,77 +33,58 @@ module.exports = (isAuthenticated) => {
   router.post('/login', (req, res) => {
     const { username, password } = req.body;
     const query = 'SELECT * FROM User WHERE Username = ? AND password = ?';
-    
+  
     db.get(query, [username, password], (err, user) => {
-        if (err) {
-            console.error('Database query error:', err.message);
-            res.status(500).json({ error: 'Internal Server Error' });
-            return;
-        }
-
-        if (user) {
-            console.log('User:', user);
-
-            if (user.UserType === 'nutritionist') {
-                // Check NutritionistSignUp table for approval status
-                const nutritionistQuery = 'SELECT * FROM NutritionistSignUp WHERE UserID = ?';
-                db.get(nutritionistQuery, [user.UserID], (err, nutritionistData) => {
-                    if (err) {
-                        console.error('Database query error:', err.message);
-                        res.status(500).json({ error: 'Internal Server Error' });
-                        return;
-                    }
-
-                    if (nutritionistData && nutritionistData.Status === 'approved') {
-                        console.log('Login successful for approved nutritionist');
-                        req.session.userId = user.UserID;
-                        res.cookie('connect.sid', req.sessionID, {
-                            maxAge: null,/* set your desired max age in milliseconds */
-                            httpOnly: false,
-                            secure: process.env.NODE_ENV === 'production', // Set to true if using HTTPS in production
-                            sameSite: 'None', // Required for cross-site cookies
-                            domain: '.vercel.app',
-                        });
-                        res.json({ message: 'Login successful', user, userType: 'nutritionist', status: 'approved' });
-                    } else if (nutritionistData && nutritionistData.Status !== 'approved') {
-                        console.log('Nutritionist account is pending approval');
-                        res.status(401).json({ error: 'Nutritionist account is pending approval' });
-                    } else {
-                        console.log('Nutritionist account not found');
-                        res.status(401).json({ error: 'Invalid credentials for a nutritionist account' });
-                    }
-                });
-            } else if (user.UserType === 'system admin') {
-                console.log('Login successful for system admin');
-                req.session.userId = user.UserID;
-                res.cookie('connect.sid', req.sessionID, {
-                    maxAge: null,/* set your desired max age in milliseconds */
-                    httpOnly: false,
-                    secure: process.env.NODE_ENV === 'production', // Set to true if using HTTPS in production
-                    sameSite: 'None', // Required for cross-site cookies
-                    domain: '.vercel.app',
-                });
-                res.json({ message: 'Login successful', user, userType: 'system admin' });
-            } else {
-                console.log('Login successful for non-nutritionist user');
-                // For non-nutritionist users, proceed with login
-                req.session.userId = user.UserID;
-                res.cookie('connect.sid', req.sessionID, {
-                    maxAge: null,/* set your desired max age in milliseconds */
-                    httpOnly: false,
-                    secure: process.env.NODE_ENV === 'production', // Set to true if using HTTPS in production
-                    sameSite: 'None', // Required for cross-site cookies
-                    domain: '.vercel.app',
-                });
-                res.json({ message: 'Login successful', user, userType: 'user' });
+      if (err) {
+        console.error('Database query error:', err.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+        return;
+      }
+  
+      if (user) {
+        console.log('User:', user);
+  
+        // Generate a JWT
+        const token = jwt.sign({ userId: user.UserID, userType: user.UserType },  secretKey, {
+          expiresIn: '1h', // Token expiration time
+        });
+  
+        if (user.UserType === 'nutritionist') {
+          // Check NutritionistSignUp table for approval status
+          const nutritionistQuery = 'SELECT * FROM NutritionistSignUp WHERE UserID = ?';
+          db.get(nutritionistQuery, [user.UserID], (err, nutritionistData) => {
+            if (err) {
+              console.error('Database query error:', err.message);
+              res.status(500).json({ error: 'Internal Server Error' });
+              return;
             }
+  
+            if (nutritionistData && nutritionistData.Status === 'approved') {
+              console.log('Login successful for approved nutritionist');
+              res.json({ message: 'Login successful', user, userType: 'nutritionist', status: 'approved', token });
+            } else if (nutritionistData && nutritionistData.Status !== 'approved') {
+              console.log('Nutritionist account is pending approval');
+              res.status(401).json({ error: 'Nutritionist account is pending approval' });
+            } else {
+              console.log('Nutritionist account not found');
+              res.status(401).json({ error: 'Invalid credentials for a nutritionist account' });
+            }
+          });
+        } else if (user.UserType === 'system admin') {
+          console.log('Login successful for system admin');
+          res.json({ message: 'Login successful', user, userType: 'system admin', token });
         } else {
-            console.log('Invalid credentials');
-            res.status(401).json({ error: 'Invalid credentials' });
+          console.log('Login successful for non-nutritionist user');
+          res.json({ message: 'Login successful', user, userType: 'user', token });
         }
-
+      } else {
+        console.log('Invalid credentials');
+        res.status(401).json({ error: 'Invalid credentials' });
+      }
     });
-});
+  });
+  
+  
   
 
   // Route for user logout
@@ -116,6 +95,11 @@ module.exports = (isAuthenticated) => {
         res.status(500).json({ error: 'Internal Server Error' });
         return;
       }
+
+      // Clear the cookie upon logout
+      res.clearCookie('sessionCookie', {
+      });
+
       res.json({ message: 'Logout successful' });
     });
   });
